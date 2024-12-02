17cf39a6a1eb697bf4f0f8e5f4b69a0fa780e33a
Fabien Airaud <fabien.airaud@etu.unice.fr>
2024-12-02 09:15:59 +0100

    Add scenario 1 and 2

---
+++ external/langium/arduino-ml/generated/2.ino
@@ -0,0 +1,31 @@
+// Generated code for Scenario1
+
+enum State {Off,On,};
+
+State currentState = Off;
+
+int lastbutton1 = 0;
+int lastbutton2 = 0;
+void setup() {
+  Serial.begin(9600);
+  pinMode(10, INPUT);
+  pinMode(12, INPUT);
+  pinMode(11, OUTPUT);
+  lastbutton1 = digitalRead(10);
+  lastbutton2 = digitalRead(12);
+}
+
+void loop() {
+  if (currentState == Off) {
+    analogWrite(11, 0);
+    if ((digitalRead(10) == HIGH and digitalRead(12) == HIGH)) {
+      currentState = On;
+    }
+  }
+  if (currentState == On) {
+    analogWrite(11, 800);
+    if ((digitalRead(10) == LOW and digitalRead(12) == LOW)) {
+      currentState = Off;
+    }
+  }
+}
